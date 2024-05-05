9525575c28166c2ec3b9806db9993dcc2901dc3b
mubinanusratullayeva <mubinanusratullayeva1.@gmail.com>
2024-05-05 14:06:14 +0500

    added Model view for permission and authentications for each models

--- music/urls.py
+++ music/urls.py
@@ -5,24 +5,28 @@ from rest_framework.authtoken import views
 
 from .views import (
     LandingPageAPIView,
-    ArtistsAPIView,
-    ArtistsDetailAPIView,
-    AlbumsAPIView,
-    AlbumsDetailAPIView,
-    SongsAPIViewSet
+    # ArtistsAPIView,
+    # ArtistsDetailAPIView,
+    # AlbumsAPIView,
+    # AlbumsDetailAPIView,
+    SongsAPIViewSet,
+    AlbumsAPIViewSet,
+    ArtistsAPIViewSet
 )
 
 
 routers = DefaultRouter()
 routers.register("songs", viewset=SongsAPIViewSet)
+routers.register("albums", viewset=AlbumsAPIViewSet)
+routers.register("artists", viewset=ArtistsAPIViewSet)
 
 
 urlpatterns = [
     path('landing/', LandingPageAPIView.as_view(), name="landing"),
-    path('artists/', ArtistsAPIView.as_view(), name="artists"),
-    path('artists/<int:id>/', ArtistsDetailAPIView.as_view(), name="artist-detail"),
-    path('albums/', AlbumsAPIView.as_view(), name="albums"),
-    path('albums/<int:id>/', AlbumsDetailAPIView.as_view(), name="album-detail"),
+    # path('artists/', ArtistsAPIView.as_view(), name="artists"),
+    # path('artists/<int:id>/', ArtistsDetailAPIView.as_view(), name="artist-detail"),
+    # path('albums/', AlbumsAPIView.as_view(), name="albums"),
+    # path('albums/<int:id>/', AlbumsDetailAPIView.as_view(), name="album-detail"),
     path('', include(routers.urls)),
     path('auth/', views.obtain_auth_token)
 ]
--- music/views.py
+++ music/views.py
@@ -15,106 +15,120 @@ class LandingPageAPIView(APIView):
         return Response(data={"message": "Hi lazy developer"})
 
 
-class ArtistsAPIView(APIView):
-    def get(self, request):
-        artists = Artist.objects.all()
-        serializer = ArtistsSerializer(artists, many=True)
-
-        return Response(data=serializer.data)
-
-    def post(self, request):
-        serializer = ArtistsSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class ArtistsAPIViewSet(ModelViewSet):
+    queryset = Artist.objects.all()
+    serializer_class = ArtistSerializer
+    permission_classes = (IsAuthenticated, )
+    authentication_classes = (TokenAuthentication, )
 
 
-class ArtistsDetailAPIView(APIView):
-    def get(self, request, id):
-        try:
-            artist = Artist.objects.get(id=id)
-            serializer = ArtistsSerializer(artist)
-
-            return Response(data=serializer.data)
-        except:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-    def patch(self, request, id):
-        artist = Artist.objects.get(id=id)
-        serializer = ArtistsSerializer(instance=artist, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(data=serializer.data, status=status.HTTP_200_OK)
-
-        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def put(self, request, id):
-        artist = Artist.objects.get(id=id)
-        serializer = ArtistsSerializer(instance=artist, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(data=serializer.data, status=status.HTTP_200_OK)
-
-        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, id):
-        artist = Artist.objetcs.get(id=id)
-        artist.delete()
-
-        return Response(status=status.HTTP_204_NO_CONTENT)
+# class ArtistsAPIView(APIView):
+#     def get(self, request):
+#         artists = Artist.objects.all()
+#         serializer = ArtistsSerializer(artists, many=True)
+#
+#         return Response(data=serializer.data)
+#
+#     def post(self, request):
+#         serializer = ArtistsSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class AlbumsAPIView(APIView):
-    def get(self, request):
-        albums = Album.objects.all()
-        serializer = AlbumsSerializer(albums, many=True)
+# class ArtistsDetailAPIView(APIView):
+#     def get(self, request, id):
+#         try:
+#             artist = Artist.objects.get(id=id)
+#             serializer = ArtistsSerializer(artist)
+#
+#             return Response(data=serializer.data)
+#         except:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#
+#     def patch(self, request, id):
+#         artist = Artist.objects.get(id=id)
+#         serializer = ArtistsSerializer(instance=artist, data=request.data, partial=True)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(data=serializer.data, status=status.HTTP_200_OK)
+#
+#         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def put(self, request, id):
+#         artist = Artist.objects.get(id=id)
+#         serializer = ArtistsSerializer(instance=artist, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(data=serializer.data, status=status.HTTP_200_OK)
+#
+#         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self, request, id):
+#         artist = Artist.objetcs.get(id=id)
+#         artist.delete()
+#
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
-        return Response(data=serializer.data)
 
-    def post(self, request):
-        serializer = AlbumsSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+# class AlbumsAPIView(APIView):
+#     def get(self, request):
+#         albums = Album.objects.all()
+#         serializer = AlbumsSerializer(albums, many=True)
+#
+#         return Response(data=serializer.data)
+#
+#     def post(self, request):
+#         serializer = AlbumsSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class AlbumsAPIViewSet(ModelViewSet):
+    queryset = Album.objects.all()
+    serializer_class = AlbumSerializer
+    permission_classes = (IsAuthenticated, )
+    authentication_classes = (TokenAuthentication, )
 
 
-class AlbumsDetailAPIView(APIView):
-    def get(self, request, id):
-        try:
-            album = Album.objects.get(id=id)
-            serializer = AlbumsSerializer(album)
-
-            return Response(data=serializer.data)
-        except:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-    def patch(self, request, id):
-        album = Album.objects.get(id=id)
-        serializer = AlbumsSerializer(instance=album, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(data=serializer.data, status=status.HTTP_200_OK)
-
-        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def put(self, request, id):
-        album = Album.objects.get(id=id)
-        serializer = AlbumsSerializer(instance=album, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(data=serializer.data, status=status.HTTP_200_OK)
-
-        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, id):
-        album = Album.objetcs.get(id=id)
-        album.delete()
-
-        return Response(status=status.HTTP_204_NO_CONTENT)
+# class AlbumsDetailAPIView(APIView):
+#     def get(self, request, id):
+#         try:
+#             album = Album.objects.get(id=id)
+#             serializer = AlbumsSerializer(album)
+#
+#             return Response(data=serializer.data)
+#         except:
+#             return Response(status=status.HTTP_404_NOT_FOUND)
+#
+#     def patch(self, request, id):
+#         album = Album.objects.get(id=id)
+#         serializer = AlbumsSerializer(instance=album, data=request.data, partial=True)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(data=serializer.data, status=status.HTTP_200_OK)
+#
+#         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def put(self, request, id):
+#         album = Album.objects.get(id=id)
+#         serializer = AlbumsSerializer(instance=album, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(data=serializer.data, status=status.HTTP_200_OK)
+#
+#         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self, request, id):
+#         album = Album.objetcs.get(id=id)
+#         album.delete()
+#
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class SongsAPIViewSet(ModelViewSet):
