0dbcf5be27b999aeafc1838b6d4f8ef33c93446c
mubinanusratullayeva <mubinanusratullayeva1.@gmail.com>
2024-05-08 07:11:44 +0500

    initial step of filtering

--- music/views.py
+++ music/views.py
@@ -1,6 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework import filters
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
@@ -18,7 +19,9 @@ class LandingPageAPIView(APIView):
 class ArtistsAPIViewSet(ModelViewSet):
     queryset = Artist.objects.all()
     serializer_class = ArtistsSerializer
-    permission_classes = (IsAuthenticated, )
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ('name', )
+    # permission_classes = (IsAuthenticated, )
     authentication_classes = (TokenAuthentication, )
 
 
